3a10973011ac0c3be63985cf2b448ddb54a011cf
v.nizaeva <nizaeva@htmlacademy.ru>
2021-03-14 22:31:13 +0300

    задание 6.2

--- js/form.js
+++ js/form.js
@@ -21,3 +21,30 @@ const changePricePlaceholder = (type) => {
 changePricePlaceholder(typeSelector.value);
 
 typeSelector.onchange = onTypeChanged;
+
+
+const roomNumber = formItem.querySelector('#room_number');
+const roomCapacity = formItem.querySelector('#capacity');
+
+const validateRooms = () => {
+
+  let isValid = roomNumber.value === 100 && roomCapacity.value === 0 || roomNumber.value != 100 && roomCapacity.value != 0 && roomNumber.value >= roomCapacity.value;
+
+  if(isValid) {
+    roomCapacity.setCustomValidity('');
+  } else {
+    if(roomNumber.value == 100) {
+      roomCapacity.setCustomValidity('100 комнат только не для гостей!!!');
+    } else {
+      roomCapacity.setCustomValidity('Гостей много, а комнат мало!!!');
+    }
+  }
+};
+
+roomCapacity.onchange = () => {
+  validateRooms()
+};
+
+roomNumber.onchange = () => {
+  validateRooms()
+};
